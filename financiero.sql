de35c50f351d342506ce60946be8bcb868adae53
Jailine González <70916201+flickerjai@users.noreply.github.com>
2021-04-21 08:55:56 -0600

    Financiero: creación de tablas, faltan FKs

---
+++ financiero.sql
@@ -0,0 +1,19 @@
+create table if not exists tipocobro(
+codigo int primary key,
+descripcion varchar(30) not null,
+monto int not null
+);
+
+create table if not exists estadocobro(
+codigo int primary key,
+descripcion varchar(30) not null
+);
+
+create table if not exists cobroXestudiante(
+codigo int primary key,
+cedula int not null,
+tipoCobro int not null,
+estadoCobro int not null,
+fechaLimite date not null,
+fechaCancelacion date
+);
