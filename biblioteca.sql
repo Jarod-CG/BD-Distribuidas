7d5940f5d702f78cc1d2b37b09d7ddf7f87f1ea4
jarod <jcervantesg05@gmail.com>
2021-04-29 18:29:12 -0600

    BD's

---
+++ biblioteca.sql
@@ -0,0 +1,149 @@
+USE [biblioteca_DB]
+GO
+
+/****** Object:  Table [dbo].[Autor]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Autor](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Autor] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[AutorXLibro]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AutorXLibro](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[idAutor] [int] NOT NULL,
+	[idLibro] [int] NOT NULL,
+ CONSTRAINT [PK_AutorXLibro] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Bibliotecario]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Bibliotecario](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Bibliotecario] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Libro]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Libro](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+	[prestado] [binary](1) NOT NULL,
+ CONSTRAINT [PK_Libro] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Prestamo]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Prestamo](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[idLibro] [int] NOT NULL,
+	[idPrestatario] [int] NOT NULL,
+	[idBibliotecario] [int] NOT NULL,
+	[idTipoPrestatario] [int] NOT NULL,
+	[activo] [binary](1) NOT NULL,
+	[fechaPrestamo] [date] NOT NULL,
+	[fechaVencimiento] [date] NOT NULL,
+ CONSTRAINT [PK_Prestamo] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[TipoPrestatario]    Script Date: 29/4/2021 18:27:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TipoPrestatario](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_TipoPrestatario] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[AutorXLibro]  WITH CHECK ADD  CONSTRAINT [FK_AutorXLibro_Autor] FOREIGN KEY([idAutor])
+REFERENCES [dbo].[Autor] ([id])
+GO
+
+ALTER TABLE [dbo].[AutorXLibro] CHECK CONSTRAINT [FK_AutorXLibro_Autor]
+GO
+
+ALTER TABLE [dbo].[AutorXLibro]  WITH CHECK ADD  CONSTRAINT [FK_AutorXLibro_Libro] FOREIGN KEY([idLibro])
+REFERENCES [dbo].[Libro] ([id])
+GO
+
+ALTER TABLE [dbo].[AutorXLibro] CHECK CONSTRAINT [FK_AutorXLibro_Libro]
+GO
+
+ALTER TABLE [dbo].[Prestamo]  WITH CHECK ADD  CONSTRAINT [FK_Prestamo_Bibliotecario] FOREIGN KEY([idBibliotecario])
+REFERENCES [dbo].[Bibliotecario] ([id])
+GO
+
+ALTER TABLE [dbo].[Prestamo] CHECK CONSTRAINT [FK_Prestamo_Bibliotecario]
+GO
+
+ALTER TABLE [dbo].[Prestamo]  WITH CHECK ADD  CONSTRAINT [FK_Prestamo_Libro] FOREIGN KEY([idLibro])
+REFERENCES [dbo].[Libro] ([id])
+GO
+
+ALTER TABLE [dbo].[Prestamo] CHECK CONSTRAINT [FK_Prestamo_Libro]
+GO
+
+ALTER TABLE [dbo].[Prestamo]  WITH CHECK ADD  CONSTRAINT [FK_Prestamo_TipoPrestatario] FOREIGN KEY([idTipoPrestatario])
+REFERENCES [dbo].[TipoPrestatario] ([id])
+GO
+
+ALTER TABLE [dbo].[Prestamo] CHECK CONSTRAINT [FK_Prestamo_TipoPrestatario]
+GO
+
+
---
+++ recursosHumanos.sql
@@ -0,0 +1,75 @@
+USE [recursosHumanos_SJ_DB]
+GO
+
+/****** Object:  Table [dbo].[Campus]    Script Date: 29/4/2021 18:26:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Campus](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Campus] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Escuela]    Script Date: 29/4/2021 18:26:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Escuela](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Escuela] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Profesor]    Script Date: 29/4/2021 18:26:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Profesor](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[idCampus] [int] NOT NULL,
+	[idEscuela] [int] NOT NULL,
+	[nombre] [nvarchar](50) NOT NULL,
+	[apellido] [nvarchar](50) NOT NULL,
+	[fechaNacimiento] [date] NOT NULL,
+	[telefono] [int] NOT NULL,
+	[correo] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Profesor] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Profesor]  WITH CHECK ADD  CONSTRAINT [FK_Profesor_Campus] FOREIGN KEY([idCampus])
+REFERENCES [dbo].[Campus] ([id])
+GO
+
+ALTER TABLE [dbo].[Profesor] CHECK CONSTRAINT [FK_Profesor_Campus]
+GO
+
+ALTER TABLE [dbo].[Profesor]  WITH CHECK ADD  CONSTRAINT [FK_Profesor_Escuela] FOREIGN KEY([idEscuela])
+REFERENCES [dbo].[Escuela] ([id])
+GO
+
+ALTER TABLE [dbo].[Profesor] CHECK CONSTRAINT [FK_Profesor_Escuela]
+GO
+
+
